ad48e542181aa2ca800b31ebfddf30094d2c5ac9
hocheeseong <58063879+hocheeseong@users.noreply.github.com>
2023-12-21 11:11:30 +0800

    Update DV0101EN-Final_Assign_Part_2_Questions.py

--- DV0101EN-Final_Assign_Part_2_Questions.py
+++ DV0101EN-Final_Assign_Part_2_Questions.py
@@ -6,7 +6,7 @@
 
 import dash
 from dash import dcc
-from dash import html
+from dash import html,ctx
 from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.graph_objs as go
@@ -38,18 +38,15 @@ app.layout = html.Div([
         html.Label("Select Statistics:"),
         dcc.Dropdown(
             id='dropdown-statistics',
-            options=[
-                           {'label': 'Yearly Statistics', 'value': 'Yearly Statistics'},
-                           {'label': 'Recession Period Statistics', 'value': 'Recession Period Statistics'}
-                           ],
+            options=
             value='Select Statistics',
-            placeholder='Select a report'
+            
         )
     ]),
     html.Div(dcc.Dropdown(
             id='select-year',
             options=[{'label': i, 'value': i} for i in year_list],
-            value='Select Year',
+            value='Select-year',
         )),
     html.Div([#TASK 2.3: Add a division for output display
     html.Div(id='output-container', className='chart-grid', style={'display': 'flex'}),])
@@ -57,7 +54,7 @@ app.layout = html.Div([
 #TASK 2.4: Creating Callbacks
 # Define the callback function to update the input container based on the selected statistics
 @app.callback(
-    Output(component_id='select_year', component_property='disabled'),
+    Output(component_id='select-year', component_property='disabled'),
     Input(component_id='dropdown-statistics',component_property='value'))
 
 def update_input_container(selected_statistics):
@@ -70,7 +67,7 @@ def update_input_container(selected_statistics):
 # Define the callback function to update the input container based on the selected statistics
 @app.callback(
     Output(component_id='output-container', component_property='children'),
-    [Input(component_id='dropdown-statistics', component_property='value'), Input(component_id='selec-year', component_property='value')])
+    [Input(component_id='dropdown-statistics', component_property='value'), Input(component_id='select-year', component_property='value')])
 
 
 def update_output_container(selected_statistics, input_year):
@@ -111,9 +108,10 @@ def update_output_container(selected_statistics, input_year):
 
 # Plot 4 bar chart for the effect of unemployment rate on vehicle type and sales
         unemp = recession_data.groupby(['Vehicle_Type','unemployment_Rate'])['Automobile_Sales'].mean().reset_index()
-        R_chart4 = dcc.Graph(figure=px.histogram(
+        R_chart4 = dcc.Graph(figure=px.bar(
                                             unemp,
                                             x='unemployment_Rate',
+                                            y='Automobile_Sales',
                                             color='Vehicle_Type',
                                             title="Unemployment to Automobile Sales by Vehicle Type over Recession Period")
                             )
